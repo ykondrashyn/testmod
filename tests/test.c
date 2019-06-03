54906569d7c73ee5f1f113896795904b20393b44
EvgeniyKondrashyn <e.kondrashyn@gmail.com>
2019-06-03 13:35:17 +0300

    Add test cases

--- tests/test.c
+++ tests/test.c
@@ -11,45 +11,119 @@
 
 #define TESTMOD_IOCBUFFERRESET    _IO(TESTMOD_IOC_MAGIC, 0)
 #define TESTMOD_IOCBUFFERGET    _IO(TESTMOD_IOC_MAGIC, 1)
+#define TESTMOD_IOCLASTSTRDROP    _IO(TESTMOD_IOC_MAGIC, 2)
 
-#define TESTMOD_IOC_MAXNR 2
+#define TESTMOD_IOC_MAXNR 3
 
 
 #define BUFFER_SIZE 5
 #define ENDING_CONDITION 1
-static void *randomString(char*, int);
+char *randomString(size_t);
+char *wbuff;
+char rbuff[100];
 
 int test1(int fd);
+int test2(int fd);
+int test3(int fd);
+int test4(int fd);
 
 int main(int argc, char** argv) {
 
   int res = 0;
   int choice;
+  srand(time(NULL));
   
   
   
-  int fa = open("/dev/testmod0", O_RDWR);
-  int fb = open("/dev/testmod1", O_RDWR);
-  int fc = open("/dev/testmod2", O_RDWR);
+  int fd = open(argv[1], O_RDWR);
+   printf("Which test would you like to run? \n");
+   scanf(" %i", &choice);
+   printf("Running test %i.\n", choice);
 
-   test1(fa);
-   test1(fb);
-   test1(fc);
+   switch(choice)
+   {
+	   case 1:
+               test1(fd);
+	       break;
+	   case 2:
+               test2(fd);
+	       break;
+	   case 3:
+               test3(fd);
+	       break;
+	   case 4:
+               test4(fd);
+	       break;
+	   case 5:
+               test5(fd);
+	       break;
+	    default:
+	        printf("Wrong selection!\n");
 
-
+   }
 }
 
 int test1(int fd){
-
-  printf("Running ioctl tests\n");
+  printf("Running ioctl test\n");
   printf("\n");
-  printf("Using ioctl to get a buffersize. Current buffersize is: %d.\n", ioctl(fd,TESTMOD_IOCBUFFERGET));
-  printf("Using ioctl to reset a buffer \n");
+  printf("Removing last entry\n", ioctl(fd,TESTMOD_IOCLASTSTRDROP));
+  
+  return 0;
+
+  
+}
+
+int test2(int fd){
+  printf("Running ioctl test\n");
+  printf("Using ioctl to reset a buffer/show buffer size\n");
   ioctl(fd,TESTMOD_IOCBUFFERRESET);
-  printf("Using ioctl to get a buffersize. Current buffersize is: %d.\n", ioctl(fd,TESTMOD_IOCBUFFERGET));
+  return 0;
+
+  
+}
+
+int test3(int fd){
+  printf("Using ioctl to get a buffersize. \nCurrent buffersize is: %d.\n", ioctl(fd,TESTMOD_IOCBUFFERGET));
   printf("Check dmesg for more info \n");
   
   return 0;
 
   
+}
+
+int test4(int fd){
+  printf("This test writes random string of size %d to device\n", BUFFER_SIZE);
+  wbuff = randomString(BUFFER_SIZE);
+  printf("\nGenerated string: %s\n", wbuff);
+  write(fd, wbuff, BUFFER_SIZE);
+  return 0;
+  
+}
+
+int test5(int fd){
+  printf("This test reads %d bytes drom the device\n", BUFFER_SIZE);
+  read(fd, rbuff, sizeof(rbuff));
+  printf("Data: %s\n", rbuff);
+  return 0;
+}
+
+char *randomString(size_t length) {
+
+    static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
+    char *rndstr = NULL;
+
+    if (length) {
+        rndstr = malloc(sizeof(char) * (length +1));
+
+        if (rndstr) {
+            for (int n = 0;n < length;n++) {
+                int key = rand() % (int)(sizeof(charset) -1);
+                rndstr[n] = charset[key];
+            }
+
+            rndstr[length] = '\0';
+        }
+    }
+
+    return rndstr;
 }
