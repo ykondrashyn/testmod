42caef5fc6f6013bc4b96989a73d00a479a88d6a
EvgeniyKondrashyn <e.kondrashyn@gmail.com>
2019-08-06 18:22:21 +0300

    Fix formatting

--- testmod.c
+++ testmod.c
@@ -46,137 +46,137 @@ MODULE_LICENSE("GPL");
 
 struct my_message                 /* String tracking */
 { 
-  unsigned int str_startpos;
-  unsigned int str_endpos;                  /* Needed in case of non-nulltermonated str */
-  struct list_head list;
+	unsigned int str_startpos;
+	unsigned int str_endpos;                  /* Needed in case of non-nulltermonated str */
+	struct list_head list;
 };
 
 struct my_buffer
 {
-    char *buffer, *end;             /* pointers to the beginning and end of the buffer */
-    int buffersize;
-    int count;                      /* keeps track of how far into the buffer we are */ 
-    int numreaders, numwriters;     /* number of readers and writers */
-    struct semaphore sem;
-    struct list_head strlist_head;  /* list for buffer indexing */
-    wait_queue_head_t queue;        /* queue of sleeping processes waiting for access to buffer */
+	char *buffer, *end;             /* pointers to the beginning and end of the buffer */
+	int buffersize;
+	int count;                      /* keeps track of how far into the buffer we are */ 
+	int numreaders, numwriters;     /* number of readers and writers */
+	struct semaphore sem;
+	struct list_head strlist_head;  /* list for buffer indexing */
+	wait_queue_head_t queue;        /* queue of sleeping processes waiting for access to buffer */
 };
 
 struct my_device {
-  struct my_buffer *wbuf;
-  struct semaphore sem;
-  struct cdev cdev;
+	struct my_buffer *wbuf;
+	struct semaphore sem;
+	struct cdev cdev;
 };
 
 /* Globals */
 int ret;
 int dev_buffer_size = BUFFER_SIZE;
-int testmod_minor =   0;
+int testmod_minor;
 int testmod_nr_devs = TESTMOD_NR_DEVS;
 dev_t testmod_p_devno;			/* first device number */
 int testmod_major =   TESTMOD_MAJOR;
 static struct my_device *testmod_device;
-static struct device* my_device = NULL;
-static struct class* my_class = NULL;
+static struct device* my_device;
+static struct class* my_class;
 static struct my_buffer *buffers;
 LIST_HEAD(messageList);
 
 /* file operations struct */
 static struct file_operations testmod_fops = {
-	      .owner   = THIS_MODULE,
-	      .read    = testmod_read,
-	      .write   = testmod_write,
-	      .open    = testmod_open,
-	      .release = testmod_release,
-        .unlocked_ioctl   = testmod_ioctl
+	.owner   = THIS_MODULE,
+	.read    = testmod_read,
+	.write   = testmod_write,
+	.open    = testmod_open,
+	.release = testmod_release,
+	.unlocked_ioctl   = testmod_ioctl
 };
 
 
 static void setup_cdev(struct my_device *dev, int count) {
 
-  int err, devno = testmod_p_devno + count;
+	int err, devno = testmod_p_devno + count;
 
-  cdev_init(&dev->cdev, &testmod_fops);
-  dev->cdev.owner = THIS_MODULE;
-  //dev->cdev.ops = &testmod_fops;
-  
-  err = cdev_add(&dev->cdev, devno, 1);
-  if (err) {
-    printk(KERN_ALERT "TESTMOD: Unable to add cdev to kernel.\n");
-  }
+	cdev_init(&dev->cdev, &testmod_fops);
+	dev->cdev.owner = THIS_MODULE;
+	//dev->cdev.ops = &testmod_fops;
 
-        printk(KERN_ALERT "Creating device testmod%u \n", count);
-        my_device = device_create(my_class, NULL, MKDEV(testmod_major, count), NULL, "testmod%u", count);
-        if (IS_ERR(my_device)) {
-            testmod_cleanup_module();
-            printk(KERN_ALERT "Failed to create testmod device.\n");
-        }
+	err = cdev_add(&dev->cdev, devno, 1);
+	if (err) {
+		printk(KERN_ALERT "TESTMOD: Unable to add cdev to kernel.\n");
+	}
+
+	printk(KERN_ALERT "Creating device testmod%u \n", count);
+	my_device = device_create(my_class, NULL, MKDEV(testmod_major, count), NULL, "testmod%u", count);
+	if (IS_ERR(my_device)) {
+		testmod_cleanup_module();
+		printk(KERN_ALERT "Failed to create testmod device.\n");
+	}
 }
 
 /* called when module is loaded */
 int testmod_init_module( void ) {
-  int i;
-  dev_t dev = 0;
+	int i;
+	dev_t dev = 0;
 	/* initialization code belongs here */
-  printk(KERN_INFO "Running init\n");
-	
+	printk(KERN_INFO "Running init\n");
+
 	ret = alloc_chrdev_region(&dev, testmod_minor, testmod_nr_devs,
-				"testmod");
-		testmod_major = MAJOR(dev);
-  if (ret < 0) {
-    printk(KERN_ALERT "TESTMOD: Failed to allocate a device region. Error %d\n", ret);
-    return ret;
-  }
-  
-  my_class = class_create(THIS_MODULE, CLASS_NAME);
-  if (IS_ERR(my_class)) {
-  	printk(KERN_WARNING "testmod: can't create class\n");
-  	ret = -ENOMEM;
-  }
+			"testmod");
+	testmod_major = MAJOR(dev);
+	if (ret < 0) {
+		printk(KERN_ALERT "TESTMOD: Failed to allocate a device region. Error %d\n", ret);
+		return ret;
+	}
 
-   testmod_p_devno = dev; 
+	my_class = class_create(THIS_MODULE, CLASS_NAME);
+	if (IS_ERR(my_class)) {
+		printk(KERN_WARNING "testmod: can't create class\n");
+		ret = -ENOMEM;
+	}
 
-  /* allocate memory for the devices */
-  testmod_device = kmalloc(testmod_nr_devs * sizeof(struct my_device), GFP_KERNEL);
-  if (testmod_device == NULL) {
-    printk(KERN_NOTICE "Unable to allocate memory!\n");
-    printk("errno = -12 ENOMEM\n");
-    unregister_chrdev_region(testmod_major, testmod_nr_devs);
-    return -ENOMEM;
-  }
-  memset(testmod_device, 0, testmod_nr_devs*sizeof(struct my_device));
-  
-  printk("Region allocated! Setting up cdev.\n");
-  
-  /* allocate memory for the buffers */
-  buffers = kmalloc(testmod_nr_devs*sizeof(struct my_buffer), GFP_KERNEL);
-  if (buffers == NULL) {
-    printk(KERN_NOTICE "Unable to allocate memory!\n");
-    printk("errno = -12 ENOMEM\n");
-    return -ENOMEM;
-  }
-  
-  for (i = 0; i < testmod_nr_devs; i++) {
-    buffers[i].buffer = kmalloc(dev_buffer_size, GFP_KERNEL);
-    //buffers[i].strlist = kmalloc(sizeof(struct my_message), GFP_KERNEL);
-    memset(buffers[i].buffer, 0, dev_buffer_size*sizeof(char));
-    setup_cdev(&testmod_device[i], i);
-    if (!buffers[i].buffer) {
-      printk(KERN_NOTICE "Unable to allocate memory!\n");
-      printk("errno = -12 ENOMEM\n");
-      return -ENOMEM;
-    }
-    buffers[i].count = 0;
-    buffers[i].numwriters = 0;
-    buffers[i].numreaders = 0;
-    buffers[i].buffersize = dev_buffer_size;
-    buffers[i].end = buffers[i].buffer + buffers[i].buffersize;
-    INIT_LIST_HEAD(&buffers[i].strlist_head);
-    init_waitqueue_head(&buffers[i].queue);
-    init_MUTEX(&buffers[i].sem);
-    init_MUTEX(&testmod_device[i].sem);
-    testmod_device[i].wbuf = &buffers[i];
-  }
+	testmod_p_devno = dev; 
+
+	/* allocate memory for the devices */
+	testmod_device = kmalloc(testmod_nr_devs * sizeof(struct my_device), GFP_KERNEL);
+	if (testmod_device == NULL) {
+		printk(KERN_NOTICE "Unable to allocate memory!\n");
+		printk("errno = -12 ENOMEM\n");
+		unregister_chrdev_region(testmod_major, testmod_nr_devs);
+		return -ENOMEM;
+	}
+	memset(testmod_device, 0, testmod_nr_devs*sizeof(struct my_device));
+
+	printk("Region allocated! Setting up cdev.\n");
+
+	/* allocate memory for the buffers */
+	buffers = kmalloc(testmod_nr_devs*sizeof(struct my_buffer), GFP_KERNEL);
+	if (buffers == NULL) {
+		printk(KERN_NOTICE "Unable to allocate memory!\n");
+		printk("errno = -12 ENOMEM\n");
+		return -ENOMEM;
+	}
+
+	for (i = 0; i < testmod_nr_devs; i++) {
+		buffers[i].buffer = kmalloc(dev_buffer_size, GFP_KERNEL);
+		//buffers[i].strlist = kmalloc(sizeof(struct my_message), GFP_KERNEL);
+		memset(buffers[i].buffer, 0, dev_buffer_size*sizeof(char));
+		setup_cdev(&testmod_device[i], i);
+		if (!buffers[i].buffer) {
+			printk(KERN_NOTICE "Unable to allocate memory!\n");
+			printk("errno = -12 ENOMEM\n");
+			return -ENOMEM;
+		}
+		buffers[i].count = 0;
+		buffers[i].numwriters = 0;
+		buffers[i].numreaders = 0;
+		buffers[i].buffersize = dev_buffer_size;
+		buffers[i].end = buffers[i].buffer + buffers[i].buffersize;
+		INIT_LIST_HEAD(&buffers[i].strlist_head);
+		init_waitqueue_head(&buffers[i].queue);
+		init_MUTEX(&buffers[i].sem);
+		init_MUTEX(&testmod_device[i].sem);
+		testmod_device[i].wbuf = &buffers[i];
+	}
 
 	printk(KERN_INFO "TESTMOD: Device initialized\n");
 	return 0;
@@ -184,51 +184,51 @@ int testmod_init_module( void ) {
 
 void testmod_cleanup_module( void ) {
 
-  int i;
-  dev_t devno = MKDEV(testmod_major, testmod_minor);
-  
-  if(!testmod_device) {
-    return;
-  }
-  for(i = 0; i<testmod_nr_devs; i++) {
-    cdev_del(&testmod_device[i].cdev);
-    kfree(buffers[i].buffer);
-    device_destroy(my_class, MKDEV(testmod_major, i));
-  }
-  class_unregister(my_class);
-  class_destroy(my_class);
-  kfree(buffers);
-  kfree(testmod_device);
-  unregister_chrdev_region(devno, testmod_nr_devs);
-  testmod_device = NULL;
+	int i;
+	dev_t devno = MKDEV(testmod_major, testmod_minor);
+
+	if(!testmod_device) {
+		return;
+	}
+	for(i = 0; i<testmod_nr_devs; i++) {
+		cdev_del(&testmod_device[i].cdev);
+		kfree(buffers[i].buffer);
+		device_destroy(my_class, MKDEV(testmod_major, i));
+	}
+	class_unregister(my_class);
+	class_destroy(my_class);
+	kfree(buffers);
+	kfree(testmod_device);
+	unregister_chrdev_region(devno, testmod_nr_devs);
+	testmod_device = NULL;
 
 	printk(KERN_INFO "TESTMOD: Module unloaded.\n");
 }
 
 
 static int testmod_open( struct inode *inode, struct file *filp ) {
-	
+
 	/* device claiming code belongs here */
 	struct my_device *dev; /* device information */
 
 	dev = container_of(inode->i_cdev, struct my_device, cdev);
-	
+
 	if(down_interruptible(&dev->sem)) {
-	  return -ERESTARTSYS;
+		return -ERESTARTSYS;
 	}
-	
+
 	filp->private_data = dev; /* for other methods */
 
 	filp->private_data = dev;
 
 	if(filp->f_mode & FMODE_READ) {
-	  dev->wbuf->numreaders++;
+		dev->wbuf->numreaders++;
 	}
 	if(filp->f_mode & FMODE_WRITE) {
-	  dev->wbuf->numwriters++;
+		dev->wbuf->numwriters++;
 	}
-        up(&dev->sem);
-  printk(KERN_INFO "TESTMOD: Device opened.\n");
+	up(&dev->sem);
+	printk(KERN_INFO "TESTMOD: Device opened.\n");
 
 	return nonseekable_open(inode,filp);
 }
@@ -237,60 +237,60 @@ static int testmod_open( struct inode *inode, struct file *filp ) {
 static int testmod_release( struct inode *inode, struct file *filp ) {
 
 	struct my_device *dev = filp->private_data;
-	
-        down(&dev->sem);
+
+	down(&dev->sem);
 	if (filp->f_mode & FMODE_READ) {
-	  dev->wbuf->numreaders--;
+		dev->wbuf->numreaders--;
 	}
 	if (filp->f_mode & FMODE_WRITE) {
-	  dev->wbuf->numwriters--;
+		dev->wbuf->numwriters--;
 	}
 	up(&dev->sem);
 
 	printk(KERN_INFO "TESTMOD: Released device.\n");
-		
+
 	return 0;
 }
 
 
 static ssize_t testmod_read( struct file *filp,
-    char *buf,      /* The buffer to fill with data     */
-    size_t count,   /* The max number of bytes to read  */
-    loff_t *f_pos )  /* The offset in the file           */
+		char *buf,      /* The buffer to fill with data     */
+		size_t count,   /* The max number of bytes to read  */
+		loff_t *f_pos )  /* The offset in the file           */
 {
-	
+
 	int data = 0;
 	unsigned long rval;
 	struct my_device *dev = filp->private_data;
 	size_t cnt;
-	
-	
+
+
 	if(down_interruptible(&(dev->wbuf->sem))) {
-	  return -ERESTARTSYS;
+		return -ERESTARTSYS;
 	}
 
 	while (dev->wbuf->count == 0) {
-	  printk(KERN_INFO "TESTMOD: Buffer is empty.\n");
-	  /* buffer is empty, sleep */
-	  up(&(dev->wbuf->sem));
-	  wake_up_interruptible(&(dev->wbuf->queue));
-	  if (filp->f_flags & O_NONBLOCK) {
-	    return -EAGAIN;
-	  }
-	  /* Wait for more data. */
-	  if (wait_event_interruptible(dev->wbuf->queue, (dev->wbuf->count != 0))) {
-	    return -ERESTARTSYS;
-	  }
-	  /* reacquire lock and loop */
-	  if(down_interruptible(&(dev->wbuf->sem))) {
-	  return -ERESTARTSYS;
-	  }
+		printk(KERN_INFO "TESTMOD: Buffer is empty.\n");
+		/* buffer is empty, sleep */
+		up(&(dev->wbuf->sem));
+		wake_up_interruptible(&(dev->wbuf->queue));
+		if (filp->f_flags & O_NONBLOCK) {
+			return -EAGAIN;
+		}
+		/* Wait for more data. */
+		if (wait_event_interruptible(dev->wbuf->queue, (dev->wbuf->count != 0))) {
+			return -ERESTARTSYS;
+		}
+		/* reacquire lock and loop */
+		if(down_interruptible(&(dev->wbuf->sem))) {
+			return -ERESTARTSYS;
+		}
 	}
-       
-        if (count > (dev->wbuf->count - *f_pos))
+
+	if (count > (dev->wbuf->count - *f_pos))
 		count = dev->wbuf->count - *f_pos;
 	rval = copy_to_user(buf, dev->wbuf->buffer + *f_pos, count);
-        if (rval < 0)
+	if (rval < 0)
 		return -EFAULT;
 	cnt = count - rval;
 	*f_pos += cnt;
@@ -304,85 +304,85 @@ static ssize_t testmod_read( struct file *filp,
 
 
 static ssize_t testmod_write( struct file *filp,
-    const char *buf,/* The buffer to get data from      */
-    size_t count,   /* The max number of bytes to write */
-    loff_t *f_pos )  /* The offset in the file           */
+		const char *buf,/* The buffer to get data from      */
+		size_t count,   /* The max number of bytes to write */
+		loff_t *f_pos )  /* The offset in the file           */
 {
 
 	int data = 0;
 	struct my_device *dev = filp->private_data;
 	struct my_message *str = kmalloc(sizeof(struct my_message), GFP_KERNEL), *ptr;
-        struct list_head *pos;
+	struct list_head *pos;
 	str->str_startpos = dev->wbuf->count;
-	
+
 	if(down_interruptible(&(dev->wbuf->sem))) {
-	  return -ERESTARTSYS;
+		return -ERESTARTSYS;
 	}
 
 	while (data < count) {
-	  /* not all data is written to buffer yet */ 
-	  while(dev->wbuf->count == dev_buffer_size) {
-	    /* buffer is full, prepare for sleep */
-	    up(&(dev->wbuf->sem));
-	    /* data is in buffer, wake sleeping processes */
-	    wake_up_interruptible(&(dev->wbuf->queue));
-	    
-	    if(filp->f_flags & O_NONBLOCK) {
-	      return -EAGAIN;
-	    }
-	    /* Wait for more space in buffer. */
-	    if(wait_event_interruptible(dev->wbuf->queue, (dev->wbuf->count < dev_buffer_size))) {
-	      return -ERESTARTSYS;
-	    }
-	    /* reacquire lock and loop */
-	    if (down_interruptible(&(dev->wbuf->sem))) {
-	      return -ERESTARTSYS;
-	    }
-	  }
-	  /* loop and write until there's no more data or no more space, one character at a time */
-	  ret = copy_from_user(dev->wbuf->buffer + dev->wbuf->count, buf + data, 1);
-  	if(ret) {
-  	  up(&(dev->wbuf->sem));
-  	  return -EFAULT;
-  	}
-  	data++;
-  	dev->wbuf->count++;
-  	if(dev->wbuf->count == dev_buffer_size) {
-  	  dev->wbuf->count = 0;
-  	}
+		/* not all data is written to buffer yet */ 
+		while(dev->wbuf->count == dev_buffer_size) {
+			/* buffer is full, prepare for sleep */
+			up(&(dev->wbuf->sem));
+			/* data is in buffer, wake sleeping processes */
+			wake_up_interruptible(&(dev->wbuf->queue));
+
+			if(filp->f_flags & O_NONBLOCK) {
+				return -EAGAIN;
+			}
+			/* Wait for more space in buffer. */
+			if(wait_event_interruptible(dev->wbuf->queue, (dev->wbuf->count < dev_buffer_size))) {
+				return -ERESTARTSYS;
+			}
+			/* reacquire lock and loop */
+			if (down_interruptible(&(dev->wbuf->sem))) {
+				return -ERESTARTSYS;
+			}
+		}
+		/* loop and write until there's no more data or no more space, one character at a time */
+		ret = copy_from_user(dev->wbuf->buffer + dev->wbuf->count, buf + data, 1);
+		if(ret) {
+			up(&(dev->wbuf->sem));
+			return -EFAULT;
+		}
+		data++;
+		dev->wbuf->count++;
+		//if(dev->wbuf->count == dev_buffer_size) {
+		//  dev->wbuf->count = 0;
+		//}
 	}
 	str->str_endpos = dev->wbuf->count;
 	list_add_tail(&str->list, &dev->wbuf->strlist_head);
 
-        /* DEBUG
+	/* DEBUG
 	 *
 	 * last
-	ptr = list_last_entry(&dev->wbuf->strlist_head, struct my_message, list);
-	printk(KERN_INFO "entry: %d %d.\n", ptr->str_startpos, ptr->str_endpos);
-        * all
-	list_for_each(pos, &dev->wbuf->strlist_head){
-	ptr = list_entry(pos, struct my_message, list);
-	printk(KERN_INFO "entry: %d %d.\n", qwe);
-	printk(KERN_INFO "entry: %d %d.\n", ptr->str_startpos, ptr->str_endpos);
-	}
-        */
-        char qwe[10];
+	 ptr = list_last_entry(&dev->wbuf->strlist_head, struct my_message, list);
+	 printk(KERN_INFO "entry: %d %d.\n", ptr->str_startpos, ptr->str_endpos);
+	 * all
+	 list_for_each(pos, &dev->wbuf->strlist_head){
+	 ptr = list_entry(pos, struct my_message, list);
+	 printk(KERN_INFO "entry: %d %d.\n", qwe);
+	 printk(KERN_INFO "entry: %d %d.\n", ptr->str_startpos, ptr->str_endpos);
+	 }
+	 */
+	char qwe[10];
 	ptr = list_last_entry(&dev->wbuf->strlist_head, struct my_message, list);
 	printk(KERN_INFO "entry: %d %d.\n", ptr->str_startpos, ptr->str_endpos);
 	//memcpy(qwe, dev->wbuf->buffer + ptr->str_startpos, (ptr->str_endpos - ptr->str_startpos)); 
-        //printk(KERN_INFO "Removing entry qwe: %s\n", qwe);
+	//printk(KERN_INFO "Removing entry qwe: %s\n", qwe);
 
 	up(&(dev->wbuf->sem));
 	/* data has been written to buffer, wake up any processes */
 	wake_up_interruptible(&dev->wbuf->queue);
-	
+
 	return data;
 }
 
 long testmod_ioctl( 
-    struct file *filp, 
-    unsigned int cmd,   /* command passed from the user */
-    unsigned long arg ) /* argument of the command */
+		struct file *filp, 
+		unsigned int cmd,   /* command passed from the user */
+		unsigned long arg ) /* argument of the command */
 {
 	struct my_device *dev = filp->private_data;
 	struct my_message *ptr;
@@ -397,31 +397,31 @@ long testmod_ioctl(
 	else if (_IOC_DIR(cmd) & _IOC_WRITE)
 		err =  !access_ok(VERIFY_READ, (void __user *)arg, _IOC_SIZE(cmd)); 
 	if (err) return -EFAULT;
-  
-	switch(cmd) {
-    
-    case TESTMOD_IOCBUFFERRESET:    /* reset buffer */
-          printk(KERN_NOTICE "Clearing buffer\n");
-          dev->wbuf->count = 0;
-          dev->wbuf->numwriters = 0;
-          dev->wbuf->numreaders = 0;
-          dev->wbuf->buffersize = dev_buffer_size;
-	  if (memset(dev->wbuf->buffer, 0, dev_buffer_size*sizeof(char)))
-	      return 0;
-          else 
-	      return -EFAULT;
 
-    	
-    case TESTMOD_IOCBUFFERGET:
-      printk(KERN_NOTICE "Buffer size: %d\n", (dev->wbuf->buffersize - dev->wbuf->count));
-      return (dev->wbuf->buffersize - dev->wbuf->count);
-      
-    case TESTMOD_IOCLASTSTRDROP:
-       ptr = list_last_entry(&dev->wbuf->strlist_head, struct my_message, list);
-       dev->wbuf->count = dev->wbuf->count - (ptr->str_endpos - ptr->str_startpos);
-       memset(dev->wbuf->buffer + ptr->str_startpos, 0, (ptr->str_endpos - ptr->str_startpos));
-       list_del(&ptr->list);
-       kfree(ptr);
+	switch(cmd) {
+
+		case TESTMOD_IOCBUFFERRESET:    /* reset buffer */
+			printk(KERN_NOTICE "Clearing buffer\n");
+			dev->wbuf->count = 0;
+			dev->wbuf->numwriters = 0;
+			dev->wbuf->numreaders = 0;
+			dev->wbuf->buffersize = dev_buffer_size;
+			if (memset(dev->wbuf->buffer, 0, dev_buffer_size*sizeof(char)))
+				return 0;
+			else 
+				return -EFAULT;
+
+
+		case TESTMOD_IOCBUFFERGET:
+			printk(KERN_NOTICE "Buffer size: %d\n", (dev->wbuf->buffersize - dev->wbuf->count));
+			return (dev->wbuf->buffersize - dev->wbuf->count);
+
+		case TESTMOD_IOCLASTSTRDROP:
+			ptr = list_last_entry(&dev->wbuf->strlist_head, struct my_message, list);
+			dev->wbuf->count = dev->wbuf->count - (ptr->str_endpos - ptr->str_startpos);
+			memset(dev->wbuf->buffer + ptr->str_startpos, 0, (ptr->str_endpos - ptr->str_startpos));
+			list_del(&ptr->list);
+			kfree(ptr);
 
 	}
 	//return retval;
--- tests/test.c
+++ tests/test.c
@@ -29,101 +29,101 @@ int test4(int fd);
 
 int main(int argc, char** argv) {
 
-  int res = 0;
-  int choice;
-  srand(time(NULL));
-  
-  
-  
-  int fd = open(argv[1], O_RDWR);
-   printf("Which test would you like to run? \n");
-   scanf(" %i", &choice);
-   printf("Running test %i.\n", choice);
+	int res = 0;
+	int choice;
+	srand(time(NULL));
 
-   switch(choice)
-   {
-	   case 1:
-               test1(fd);
-	       break;
-	   case 2:
-               test2(fd);
-	       break;
-	   case 3:
-               test3(fd);
-	       break;
-	   case 4:
-               test4(fd);
-	       break;
-	   case 5:
-               test5(fd);
-	       break;
-	    default:
-	        printf("Wrong selection!\n");
 
-   }
+
+	int fd = open(argv[1], O_RDWR);
+	printf("Which test would you like to run? \n");
+	scanf(" %i", &choice);
+	printf("Running test %i.\n", choice);
+
+	switch(choice)
+	{
+		case 1:
+			test1(fd);
+			break;
+		case 2:
+			test2(fd);
+			break;
+		case 3:
+			test3(fd);
+			break;
+		case 4:
+			test4(fd);
+			break;
+		case 5:
+			test5(fd);
+			break;
+		default:
+			printf("Wrong selection!\n");
+
+	}
 }
 
 int test1(int fd){
-  printf("Running ioctl test\n");
-  printf("\n");
-  printf("Removing last entry\n", ioctl(fd,TESTMOD_IOCLASTSTRDROP));
-  
-  return 0;
+	printf("Running ioctl test\n");
+	printf("\n");
+	printf("Removing last entry\n", ioctl(fd,TESTMOD_IOCLASTSTRDROP));
 
-  
+	return 0;
+
+
 }
 
 int test2(int fd){
-  printf("Running ioctl test\n");
-  printf("Using ioctl to reset a buffer/show buffer size\n");
-  ioctl(fd,TESTMOD_IOCBUFFERRESET);
-  return 0;
+	printf("Running ioctl test\n");
+	printf("Using ioctl to reset a buffer/show buffer size\n");
+	ioctl(fd,TESTMOD_IOCBUFFERRESET);
+	return 0;
 
-  
+
 }
 
 int test3(int fd){
-  printf("Using ioctl to get a buffersize. \nCurrent buffersize is: %d.\n", ioctl(fd,TESTMOD_IOCBUFFERGET));
-  printf("Check dmesg for more info \n");
-  
-  return 0;
+	printf("Using ioctl to get a buffersize. \nCurrent buffersize is: %d.\n", ioctl(fd,TESTMOD_IOCBUFFERGET));
+	printf("Check dmesg for more info \n");
 
-  
+	return 0;
+
+
 }
 
 int test4(int fd){
-  printf("This test writes random string of size %d to device\n", BUFFER_SIZE);
-  wbuff = randomString(BUFFER_SIZE);
-  printf("\nGenerated string: %s\n", wbuff);
-  write(fd, wbuff, BUFFER_SIZE);
-  return 0;
-  
+	printf("This test writes random string of size %d to device\n", BUFFER_SIZE);
+	wbuff = randomString(BUFFER_SIZE);
+	printf("\nGenerated string: %s\n", wbuff);
+	write(fd, wbuff, BUFFER_SIZE);
+	return 0;
+
 }
 
 int test5(int fd){
-  printf("This test reads %d bytes drom the device\n", BUFFER_SIZE);
-  read(fd, rbuff, sizeof(rbuff));
-  printf("Data: %s\n", rbuff);
-  return 0;
+	printf("This test reads %d bytes drom the device\n", BUFFER_SIZE);
+	read(fd, rbuff, sizeof(rbuff));
+	printf("Data: %s\n", rbuff);
+	return 0;
 }
 
 char *randomString(size_t length) {
 
-    static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
-    char *rndstr = NULL;
+	static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
+	char *rndstr = NULL;
 
-    if (length) {
-        rndstr = malloc(sizeof(char) * (length +1));
+	if (length) {
+		rndstr = malloc(sizeof(char) * (length +1));
 
-        if (rndstr) {
-            for (int n = 0;n < length;n++) {
-                int key = rand() % (int)(sizeof(charset) -1);
-                rndstr[n] = charset[key];
-            }
+		if (rndstr) {
+			for (int n = 0;n < length;n++) {
+				int key = rand() % (int)(sizeof(charset) -1);
+				rndstr[n] = charset[key];
+			}
 
-            rndstr[length] = '\0';
-        }
-    }
+			rndstr[length] = '\0';
+		}
+	}
 
-    return rndstr;
+	return rndstr;
 }
